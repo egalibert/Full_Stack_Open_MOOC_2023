98a944a616f4661fa3e2c38a921b2c8ac0409d7a
Elliot Galibert <elliotgalibert@gmail.com>
2023-05-25 11:59:37 +0300

    first ex of part1 done

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,38 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = (props) => {
+    return(
+      <h1>{course}</h1>
+    )
+  }
+  const Content = (props) => {
+    return(
+      <div>
+        <p>{part1} {exercises1}</p>
+        <p>{part2} {exercises2}</p>
+        <p>{part3} {exercises3}</p>
+      </div>
+    )
+  }
+  const Total = (props) => {
+    return(
+      <p>number of exercises {exercises1 + exercises2 + exercises3}</p>
+    )
+  }
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1,part2,part3]} />
+      <Total parts={[part1,part2,part3]} />
+    </div>
+  )
+}
+
+export default App
