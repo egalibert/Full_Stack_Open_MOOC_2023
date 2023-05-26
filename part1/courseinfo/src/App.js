fe7f85d68eb36df43e19666ba298abc8bb575d2c
Elliot Galibert <elliotgalibert@gmail.com>
2023-05-26 11:20:16 +0300

    part 1.3 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,16 +1,22 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Part = (props) => {
     return (
       <p>
-        {props.kurssi} {props.exercises}
+        {props.kurssi} {props.ex}
       </p>
     )
   }
@@ -23,15 +29,15 @@ const App = () => {
   const Content = (props) => {
     return(
       <div>
-      <Part kurssi={part1} exercises={exercises1}/>
-      <Part kurssi={part2} exercises={exercises2}/>
-      <Part kurssi={part3} exercises={exercises3}/>
+      <Part kurssi={part1.name} ex={part1.exercises}/>
+      <Part kurssi={part2.name} ex={part2.exercises}/>
+      <Part kurssi={part3.name} ex={part3.exercises}/>
       </div>
     )
   }
   const Total = (props) => {
     return(
-      <p>number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     )
   }
   return (
